b8dab3acb61c1fa34fcae1a4cb5ce253e8bc1de9
CSAndrewZ <103061288+CSAndrewZ@users.noreply.github.com>
2022-05-02 21:10:55 -0400

    Update InsertMarketing.sql

--- InsertMarketing.sql
+++ InsertMarketing.sql
@@ -9,12 +9,12 @@ VALUES
     (2, 2,  N'Sports'),
     (3, 2,  N'Tourer'),
     (4, 3,  N'Sports'),
-    (5, 4, , N'Sports')
+    (5, 4,  N'Sports')
 ,
     (6, 4, N'Aston Martin', N'Tourer'),
-    (7, 5, , N'Tourer') ,
-    (8, 5, , N'Chauffer'),
-    (9, 5, , N'Prestige')
+    (7, 5,  N'Tourer') ,
+    (8, 5,  N'Chauffer'),
+    (9, 5,  N'Prestige')
 ,
     (10, 6,  N'Chauffer'),
     (11, 6,  N'Prestige'),
