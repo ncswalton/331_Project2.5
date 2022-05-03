6f891074fb3507a6699ff070d18233a000d87c73
NickWalton <nicholas.walton@anikasystems.com>
2022-05-03 00:56:27 -0400

    Update AddColumnsProcedure

--- AddColumnsProcedure.sql
+++ AddColumnsProcedure.sql
@@ -11,15 +11,11 @@ as
 begin
 
 -- drop customerId now to avoid issues later
-exec Udt.dropCustomerId
+
 ---- renaming
 exec sp_rename '[Data].Country.CountryISO2', 'CountryCode', 'COLUMN';
 exec sp_rename '[Data].Customer.Town', 'City', 'COLUMN';
 exec sp_rename '[Data].Customer.Country', 'CountryCode', 'COLUMN';
-
--- create PKs
--- have to drop the CustomerID column first
---
 
 alter table data.country
 add CountryId Udt.PrestigeSmallInt identity(1,1)
@@ -29,13 +25,6 @@ add primary key([CountryId])
 
 alter table [data].Make
 add CountryId Udt.PrestigeSmallInt not null default(0)
-
--- commenting out customerId
--- alter table [data].customer
--- add CustomerId Udt.PrestigeSmallInt identity(1,1);
-
---alter table [data].customer
---add primary key([CustomerId])
 
 alter table data.country
 add SalesRegionId Udt.PrestigeSmallInt not null
@@ -51,23 +40,16 @@ add primary key([StockID])
 update [data].Make
 set [MakeCountry] = replace([MakeCountry], 'GER', 'DEU')
 
-
 -- Delete erroneous rows from SalesDetail and 1 erroneous row from Stock
-
--- Delete from [Data].[SalesDetails] where StockID = 205 or StockID = 206 or StockID = 244
 
 Delete from [Data].[Stock] where StockID = 395
 
+-- Delete from [Data].[SalesDetails] where StockID = 205 or StockID = 206 or StockID = 244
 delete from [data].Stock where StockCode = '1A80A54C-D98D-41D6-87EE-8F67F3B06FA8'
 delete from [data].Stock where StockCode = '38264675-F235-412B-9B67-8F8CD86CF40D'
 delete from [data].Stock where StockCode = '2EE943CB-2473-4333-8626-FC94FCD0947E'
 
--- Delete from [data].[Stock] where StockID = 205 and StockID = 206 and StockID = 244
--- doing this above with StockCode
-
 --todo: delete the rest of the data
-
--- Stock - add StockID
 
 end
 
@@ -77,6 +59,4 @@ end
 
 --sales details ID: 180 181 216
 select * from [data].SalesDetails
-where StockId = 205
-or StockID = 206 
-or StockID
+where SalesID = 166 or SalesID = 167 or SalesID = 216
